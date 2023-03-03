25a1e71ff1aea30e1db8c019e8403d40aa006e5c
qugeez <awuarich60@gmail.com>
2023-03-02 16:16:43 -0800

    corrections

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,23 +1,29 @@
 #include "main.h"
-
 /**
  * _strcat - concatenates two strings
- * @src: string to copy
- * @dest: copied string
- * Return: a pointer to the resulting string @dest
+ * @dest: input value
+ * @src: input value
+ *
+ * Return: void
  */
 char *_strcat(char *dest, char *src)
 {
-	int i, j;
+	int i;
+	int j;
 
 	i = 0;
 	while (dest[i] != '\0')
+	{
 		i++;
+	}
 	j = 0;
 	while (src[j] != '\0')
 	{
 		dest[i] = src[j];
-		j++;
 		i++;
+		j++;
 	}
+
+	dest[i] = '\0';
 	return (dest);
+}
--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,20 +1,24 @@
 #include "main.h"
-
 /**
- * _strcmp - compares two strings
- * @s1: string one
- * @s2: string two
- * Return: 0 is strings are equal else, defference between strings
+ * _strcmp - compare string values
+ * @s1: input value
+ * @s2: input value
+ *
+ * Return: s1[i] - s2[i]
  */
 int _strcmp(char *s1, char *s2)
 {
-	for (; *s1 != '\0' && *s2 != '\0'; s2++)
-		if (*s1 != *s2)
-			return (*s1 - *s2);
-	if (*s1 != '\0')
-		return (*s1);
-	else if (*s2 != '\0')
-		return (*s2);
+	int i;
 
+	i = 0;
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+		i++;
+	}
 	return (0);
 }
+
