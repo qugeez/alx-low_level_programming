880cb5cb1d5c8a5e49f31da4a674a03dc3773421
qugeez <awuarich60@gmail.com>
2023-03-14 15:27:18 -0700

    correction

--- 0x09-static_libraries/0-strcat.c
+++ 0x09-static_libraries/0-strcat.c
@@ -2,8 +2,9 @@
  /**
  * _strcat - concatenates two strings
  * @dest: input value
- * @src: input valu
- * Return:void	
+ * @src: input value
+ *
+ * Return: void
  */
 char *_strcat(char *dest, char *src)
 {
@@ -22,7 +23,7 @@ char *_strcat(char *dest, char *src)
 		dest[i] = src[j];
 		i++;
 		j++;
-	}		
+	}
 
 
 	dest[i] = '\0';
